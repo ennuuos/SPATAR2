d3931303b57a6f20d96cd13d20af2171af1b4e3d
Kpaiy <craigjarrodonly@gmail.com>
2016-06-28 14:56:49 +1000

    Loop

--- master.c
+++ master.c
@@ -2,13 +2,51 @@
 
 
 //Declare any variables shared between functions here
-
+float ZRState[12];
+bool isOn;
 bool photosTaken[6];
 
+void init(){
+	//This function is called once when your code is first loaded.
+	isOn = true;
 
+	//IMPORTANT: make sure to set any variables that need an initial value.
+	//Do not assume variables will be set to 0 automatically!
+}
 
-
-
+void loop(){
+    //update self position
+    api.getMyZRState(ZRState);
+	//This function is called once per second.  Use it to control the satellite.
+	if(game.getNextFlare() == -1) {
+	    //turn on if off
+        if (!isOn) {
+            game.turnOn();
+            isOn = true;
+        }
+        //normal loop stuff
+        if (game.getMemoryFilled() == game.getMemorySize()) {
+            //upload photos if in photozone
+            upload();
+        } else {
+            //take photos
+            takePhoto();
+        }
+	}
+    else {
+        //emergency shutdown
+        if(game.getNextFlare() < 4) {
+            if (!inShadow(ZRState[0], ZRState[1], ZRState[2])) {
+                game.turnOff();
+                isOn = false;
+            }
+        }
+        //prepare for flare
+        if (game.getMemoryFilled() > 0) {
+            upload();
+        }
+	}
+}
 
 float nextPhotoPos() {
     float position[3];
@@ -29,4 +67,15 @@ int nextPhotoID() {
 void takePhoto() {
     float pos[3] = getPOILoc(pos, nextPhotoID())
     lookAt(pos, nextPhotoPos());
+}
+
+
+///UTILITY
+bool inShadow (float pointx, float pointy, float pointz) {
+	//if on shadow side of the asteroid and within yz radius
+	if (pointx > 0 && sqrtf(mathSquare(pointy) + mathSquare(pointz)) <= 0.2) {
+		return true;
+	} else {
+		return false;
+	}
 }
